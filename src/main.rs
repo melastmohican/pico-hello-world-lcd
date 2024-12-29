4a412bcc7d4fcb82093f2a555a2a6d016a8281b8
Mariusz Jurgielewicz <mordor@mail.com>
2024-12-29 13:21:20 -0800

    Working version

---
+++ src/main.rs
@@ -0,0 +1,160 @@
+//! Blinks the LED on a Pico board
+//!
+//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
+#![no_std]
+#![no_main]
+
+use bsp::entry;
+use defmt::*;
+use defmt_rtt as _;
+use embedded_hal::digital::OutputPin;
+use panic_probe as _;
+
+// Provide an alias for our BSP so we can switch targets quickly.
+// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
+use rp_pico as bsp;
+// use sparkfun_pro_micro_rp2040 as bsp;
+
+use bsp::hal::{
+    clocks::{init_clocks_and_plls, Clock},
+    fugit::RateExtU32,
+    gpio,
+    gpio::FunctionSpi,
+    pac,
+    sio::Sio,
+    spi,
+    watchdog::Watchdog,
+};
+use display_interface_spi::SPIInterface;
+use embedded_graphics::draw_target::DrawTarget;
+use embedded_graphics::mono_font::ascii::FONT_9X18;
+use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::pixelcolor::{Rgb565, RgbColor};
+use embedded_graphics::prelude::Point;
+use embedded_graphics::text::renderer::CharacterStyle;
+use embedded_graphics::{text::Text, Drawable};
+use embedded_hal::delay::DelayNs;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use mipidsi::models::ILI9341Rgb565;
+//use mipidsi::options::{ColorOrder, Orientation, Rotation};
+use mipidsi::Builder;
+
+#[entry]
+fn main() -> ! {
+    info!("Program start");
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
+
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = DelayCompat(cortex_m::delay::Delay::new(
+        core.SYST,
+        clocks.system_clock.freq().to_Hz(),
+    ));
+
+    let pins = bsp::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
+    // on-board LED, it might need to be changed.
+    //
+    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead.
+    // One way to do that is by using [embassy](https://github.com/embassy-rs/embassy/blob/main/examples/rp/src/bin/wifi_blinky.rs)
+    //
+    // If you have a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
+    // LED to one of the GPIO pins, and reference that pin here. Don't forget adding an appropriate resistor
+    // in series with the LED.
+    let mut led_pin = pins.led.into_push_pull_output();
+
+    // These are implicitly used by the spi driver if they are in the correct mode
+    let sck = pins.gpio2.into_function::<FunctionSpi>(); // SCK
+    let mosi = pins.gpio3.into_function::<FunctionSpi>(); // SCL TX
+    let miso = pins.gpio0.into_function::<FunctionSpi>(); // SDA RX
+
+    let cs = pins.gpio20.into_push_pull_output();
+    let rst = pins
+        .gpio19
+        .into_push_pull_output_in_state(gpio::PinState::High);
+    let dc = pins.gpio18.into_push_pull_output();
+
+    // Create an SPI driver instance for the SPI0 device
+    let spi = spi::Spi::<_, _, _, 8>::new(pac.SPI0, (mosi, miso, sck)).init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        400000_u32.Hz(),
+        &embedded_hal::spi::MODE_0,
+    );
+
+    let spi_device = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
+    let di = SPIInterface::new(spi_device, dc);
+
+    let mut display = Builder::new(ILI9341Rgb565, di)
+        .reset_pin(rst)
+        //.color_order(ColorOrder::Rgb)
+        //.orientation(Orientation::new().rotate(Rotation::Deg90).flip_vertical())
+        //.display_size(320, 240)
+        .init(&mut delay)
+        .unwrap();
+
+    display.clear(Rgb565::BLACK).unwrap();
+
+    // Create text style
+    let mut style = MonoTextStyle::new(&FONT_9X18, Rgb565::WHITE);
+
+    // Position x:5, y: 10
+    Text::new("Hello", Point::new(5, 10), style)
+        .draw(&mut display)
+        .unwrap();
+
+    // Turn text to blue
+    style.set_text_color(Some(Rgb565::BLUE));
+    Text::new("World", Point::new(160, 26), style)
+        .draw(&mut display)
+        .unwrap();
+
+    loop {
+        info!("on!");
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        info!("off!");
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
+    }
+}
+
+/// Wrapper around `Delay` to implement the embedded-hal 1.0 delay.
+///
+/// This can be removed when a new version of the `cortex_m` crate is released.
+struct DelayCompat(cortex_m::delay::Delay);
+
+impl embedded_hal::delay::DelayNs for DelayCompat {
+    fn delay_ns(&mut self, mut ns: u32) {
+        while ns > 1000 {
+            self.0.delay_us(1);
+            ns = ns.saturating_sub(1000);
+        }
+    }
+
+    fn delay_us(&mut self, us: u32) {
+        self.0.delay_us(us);
+    }
+}
+// End of file
